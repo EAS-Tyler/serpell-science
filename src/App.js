aabff4d0fe32b81787b925d00041922c8f2dae56
EAS-Tyler <tyler@enterpriseautomation.co.uk>
2025-11-12 14:53:22 +0000

    colouring + dot animation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import logo from './logo.jpeg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./logo.jpeg";
+import "./App.css";
 
 function App() {
+  const [dots, setDots] = useState("");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDots((prev) => (prev.length === 3 ? "" : prev + "."));
+    }, 500);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} 
-         alt="logo" 
-         width="500"
-         height="500"
-         />
-        <p>
-Work in progress...
-        </p>
-   
+      <header
+        className="App-header"
+        style={{
+          backgroundColor: "#ffffff",
+          color: "white",
+          padding: "20px",
+          textAlign: "center",
+        }}
+      >
+        <img src={logo} alt="logo" width="500" height="500" 
+        />
+        <p style={{ color: "#000000" }}>Work in progress...{dots}</p>
       </header>
     </div>
   );
