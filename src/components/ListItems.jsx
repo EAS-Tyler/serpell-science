37a72062ba0178c8e4612faa978945f25c72e93c
EAS-Tyler <tyler@enterpriseautomation.co.uk>
2025-11-20 22:28:50 +0000

    checkboxes

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -16,8 +16,7 @@ import {
   ListItemText,
   ListItemIcon
 } from "@mui/material";
-// import ListItemIcon from "@mui/material/ListItemIcon";
-// import CheckIcon from "@mui/icons-material/Check";
+import CheckIcon from "@mui/icons-material/Check";
 
 
 const serif = '"Times New Roman", Georgia, "Times", serif';
@@ -26,14 +25,9 @@ const ListItems = () => {
 
 return (
 
-    <Container maxWidth="md" sx={{ pb: 6 }}>
+    <Container maxWidth="lg" sx={{ pb: 6 }}>
             <Box sx={{ textAlign: "center", mb: 4 }}>
-              {/* <Typography
-                variant="h6"
-                sx={{ fontFamily: serif, mb: 1, fontSize: 20 }}
-              >
-                *
-              </Typography> */}
+
               <Typography
             variant="h4"
         component="h3"
@@ -47,7 +41,7 @@ return (
               </Typography>
             </Box>
     
-            <Grid container spacing={6}>
+            <Grid container spacing={4} sx={{ alignItems: "flex-start", paddingLeft: '70px' }}>
               {/* Left column */}
               <Grid item xs={12} md={6}>
                 <Typography
@@ -63,18 +57,27 @@ return (
                 </Typography>
                 <List dense sx={{ pl: 2 }}>
                   <ListItem disableGutters sx={{ alignItems: "flex-start" }}>
+                    <ListItemIcon sx={{ minWidth: 36, alignSelf: "flex-start", mt: 0.5 }}>
+                      <CheckIcon />
+                    </ListItemIcon>
                     <ListItemText
                       primary="Facilitation of communication of high level science between potential partners"
                       primaryTypographyProps={{ variant: "body2" }}
                     />
                   </ListItem>
                   <ListItem disableGutters sx={{ alignItems: "flex-start" }}>
+                    <ListItemIcon sx={{ minWidth: 36, alignSelf: "flex-start", mt: 0.5 }}>
+                      <CheckIcon />
+                    </ListItemIcon>
                     <ListItemText
                       primary="Inform policy makers of strategic scientific priorities"
                       primaryTypographyProps={{ variant: "body2" }}
                     />
                   </ListItem>
                   <ListItem disableGutters sx={{ alignItems: "flex-start" }}>
+                    <ListItemIcon sx={{ minWidth: 36, alignSelf: "flex-start", mt: 0.5 }}>
+                      <CheckIcon />
+                    </ListItemIcon>
                     <ListItemText
                       primary="Public speaking"
                       primaryTypographyProps={{ variant: "body2" }}
@@ -98,25 +101,36 @@ return (
                 </Typography>
                 <List dense sx={{ pl: 2 }}>
                   <ListItem disableGutters sx={{ alignItems: "flex-start" }}>
-                
+                    <ListItemIcon sx={{ minWidth: 36, alignSelf: "flex-start", mt: 0.5 }}>
+                      <CheckIcon />
+                    </ListItemIcon>
                     <ListItemText
                       primary="Pathways to translation and early diagnosis"
                       primaryTypographyProps={{ variant: "body2" }}
                     />
                   </ListItem>
                   <ListItem disableGutters sx={{ alignItems: "flex-start" }}>
+                    <ListItemIcon sx={{ minWidth: 36, alignSelf: "flex-start", mt: 0.5 }}>
+                      <CheckIcon />
+                    </ListItemIcon>
                     <ListItemText
                       primary="Determining research and impact priorities"
                       primaryTypographyProps={{ variant: "body2" }}
                     />
                   </ListItem>
                   <ListItem disableGutters sx={{ alignItems: "flex-start" }}>
+                    <ListItemIcon sx={{ minWidth: 36, alignSelf: "flex-start", mt: 0.5 }}>
+                      <CheckIcon />
+                    </ListItemIcon>
                     <ListItemText
                       primary="Directions for funding priorities"
                       primaryTypographyProps={{ variant: "body2" }}
                     />
                   </ListItem>
                   <ListItem disableGutters sx={{ alignItems: "flex-start" }}>
+                  <ListItemIcon sx={{ minWidth: 36, alignSelf: "flex-start", mt: 0.5 }}>
+                      <CheckIcon />
+                    </ListItemIcon>
                     <ListItemText
                       primary="Cultural changes in academic research"
                       primaryTypographyProps={{ variant: "body2" }}
